d1ff3ecc9a22e83a5eaab0ba19099714ab34e458
BBDXF <zhang206zyx@hotmail.com>
2025-05-23 09:24:14 +0800

    remove unused enter

--- tests/demo.js
+++ tests/demo.js
@@ -1,30 +1,30 @@
-console.log('demo.js log ...\n')
+console.log('demo.js log ...')
 
-console.log('==> globalThis:\n')
+console.log('==> globalThis:')
 for (var key in globalThis) {
-    console.log("  " + key + '\n')
+    console.log("  " + key)
 }
 
-console.log('==> std:\n')
+console.log('==> std:')
 for (var key in std) {
-    console.log("  " + key + '\n')
+    console.log("  " + key)
 }
 
-console.log('==> os:\n')
+console.log('==> os:')
 for (var key in os) {
-    console.log("  " + key + '\n')
+    console.log("  " + key)
 }
 
-console.log('==> bjson:\n')
+console.log('==> bjson:')
 for (var key in bjson) {
-    console.log("  " + key + '\n')
+    console.log("  " + key)
 }
 
 globalThis.std.printf('\n\nhello_world\n');
-globalThis.std.printf(globalThis + '\n');
+globalThis.std.printf(globalThis+'\n');
 
 
 var a = 0xf2;
-os.setTimeout(() => { std.printf('AAB\n') }, 2000)
+os.setTimeout(() => { std.printf('timeout: AAB\n') }, 2000)
 globalThis.std.printf("a value is %d\n", a);
 
